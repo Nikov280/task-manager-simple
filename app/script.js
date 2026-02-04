afadc7586e0a09422a6fda325854afcc9f924131
nico_ <nico_@NICOLAPTOP>
2026-02-04 14:00:20 +0000

    Initialized JS file

---
+++ app/script.js
@@ -0,0 +1,7 @@
+// Seleccionamos el botón
+const btn = document.getElementById('completeBtn');
+
+// Agregamos el evento click
+btn.addEventListener('click', function() {
+    alert("Task completed! Notification sent.");
+});
